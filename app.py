b6359ae6691189f6f22376e5dce268e0826c739b
jferreroem <75042364+jferreroem@users.noreply.github.com>
2020-11-28 20:38:07 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sys import argv
+import bottle
+from bottle import route,run
+
+bottle.debug(True)
+
+@route('/')
+def index():
+	return "<h1>Hola Mundo2!!!</h1>"
+
+if __name__ == '__main__':
+	run(host='0.0.0.0',port=argv[1])
